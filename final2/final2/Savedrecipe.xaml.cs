1821cec1aca52e32ce04643cf55d18ad51df0802
ASUS <ASUS@DESKTOP-NP91F08>
2020-04-20 00:05:47 -0600

    saved recipe update

--- final2/final2/Savedrecipe.xaml.cs
+++ final2/final2/Savedrecipe.xaml.cs
@@ -11,21 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 namespace final2
 {
     /// <summary>
